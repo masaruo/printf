1844a5a84f1f7db35db7828555314ede7a2d1d5d
masaru ogawa <masaruo@gmail.com>
2025-08-29 08:56:51 +0900

    creating core helper function

--- ft_printf.c
+++ ft_printf.c
@@ -10,30 +10,58 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fcntl.h"
 #include "ft_printf.h"
+
+// int	ft_printf(const char *fmt, ...)
+// {
+// 	va_list	ap;
+// 	size_t	n;
+// 	ssize_t	len;
+
+// 	n = 0;
+// 	len = 0;
+// 	va_start(ap, fmt);
+// 	while (fmt[n])
+// 	{
+// 		if (fmt[n] == '%')
+// 		{
+// 			n++;
+// 			len += ft_arg_handler_pf(STDOUT_FILENO, fmt[n], &ap);
+// 		}
+// 		else
+// 			len += ft_putchar_cnt_pf(STDOUT_FILENO, fmt[n]);
+// 		n++;
+// 	}
+// 	va_end(ap);
+// 	return ((int)len);
+// }
+
+int ft_xprintf(int fd, const char *fmt, va_list ap)
+{
+	return (ft_dprintf(fd, fmt, ap));
+}
+
+int	ft_sprintf(char *dst, const char *fmt, ...)
+{
+	va_list	ap;
+	int		len;
+
+	
+	va_start(ap, fmt);
+	
+	va_end(ap);
+}
 
 int	ft_printf(const char *fmt, ...)
 {
 	va_list	ap;
-	size_t	n;
-	ssize_t	len;
+	int		len;
 
-	n = 0;
-	len = 0;
 	va_start(ap, fmt);
-	while (fmt[n])
-	{
-		if (fmt[n] == '%')
-		{
-			n++;
-			len += ft_arg_handler_pf(STDOUT_FILENO, fmt[n], &ap);
-		}
-		else
-			len += ft_putchar_cnt_pf(STDOUT_FILENO, fmt[n]);
-		n++;
-	}
+	len = ft_xprintf(stdout, fmt, ap);
 	va_end(ap);
-	return ((int)len);
+	return (len);
 }
 
 int	ft_dprintf(int fd, const char *fmt, ...)
---
+++ ft_printf_utils.h
@@ -0,0 +1,12 @@
+#ifndef FT_PRINTF_UTILS_H
+# define FT_PRINTF_UTILS_H
+
+va_list;
+
+ssize_t	ft_arg_handler_pf(int fd, char c, va_list *ap);
+ssize_t	ft_putchar_cnt_pf(int fd, char c);
+ssize_t	ft_putstr_cnt_pf(int fd, char *s);
+ssize_t	ft_putnbr_pf(int fd, unsigned long n, size_t base, char *nbr);
+ssize_t	ft_putnbr_int_pf(int fd, int n);
+
+#endif
